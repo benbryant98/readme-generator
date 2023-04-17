e59f5b55dade2af5656970ff934a11091c131fbb
Ben <bryantbc98@gmail.com>
2023-04-17 10:48:14 -0400

    added questions and organized files a bit better

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 // TODO: Include packages needed for this application
-
+var inquirer = require('inquirer');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    "What motivated you to create this project?",
+    "What problem does this project solve?",
+    "Did you learn anything from the project experience?",
+    "Name some features that make this project unique.",
+    "Are there any steps required to install/implement the project?",
+    "Please provide instructions for using your application."
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
